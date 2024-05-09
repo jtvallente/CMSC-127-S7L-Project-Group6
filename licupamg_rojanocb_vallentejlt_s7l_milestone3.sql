6f7c89cc17e753a07b2424ba5c31edaeeab9353a
htttpmae <111998509+htttpmae@users.noreply.github.com>
2024-05-10 04:44:44 +0800

    Update licupamg_rojanocb_vallentejlt_s7l_milestone3.sql

--- licupamg_rojanocb_vallentejlt_s7l_milestone3.sql
+++ licupamg_rojanocb_vallentejlt_s7l_milestone3.sql
@@ -63,7 +63,203 @@ CREATE TABLE FoodReview (
 );
 
 -- DML FOR PROJECT FEATURES
+-- 1. Add, update, and delete a food review (on a food establishment or a food item);
+-- Add a food review on a food establisment or a food item, assumming there are
+-- variables for these values. Either can be, dependeng if item id is given.
+-- Food id are NULL by default
+INSERT INTO
+    FoodReview (
+        review_id,
+        user_id,
+        establishment_id,
+        review_text,
+        rating,
+        review_month,
+        review_day,
+        review_year
+    )
+VALUES
+    (
+        review_id,
+        user_id,
+        establishment_id,
+        review_text,
+        rating,
+        MONTH (CURDATE ()),
+        DAY (CURDATE ()),
+        YEAR (CURDATE ()),
+    );
 
+INSERT INTO
+    FoodReview (
+        review_id,
+        user_id,
+        establishment_id,
+        item_id,
+        review_text,
+        rating,
+        review_month,
+        review_day,
+        review_year
+    )
+VALUES
+    (
+        review_id,
+        user_id,
+        establishment_id,
+        item_id,
+        review_text,
+        rating,
+        MONTH (CURDATE ()),
+        DAY (CURDATE ()),
+        YEAR (CURDATE ()),
+    );
+
+-- Update a food review on a food establishment or food item, review id is given 
+-- and assumming there are variables for these values.
+-- Update rating
+UPDATE FoodReview
+SET
+    rating = new_rating
+WHERE
+    review_id = review_id;
+
+-- Update review
+UPDATE FoodReview
+SET
+    review_text = new_review_text
+WHERE
+    review_id = review_id;
+
+-- Update rating and review
+UPDATE FoodReview
+SET
+    rating = new_rating,
+    review_text = new_review_text
+WHERE
+    review_id = review_id;
+
+-- Delete a food review on a food establishment or food item, review id is given.
+DELETE FROM FoodReview
+WHERE
+    review_id = review_id;
+
+-- 2. Add, delete, search, and update a food establishment;
+-- Add a food establishment, assumming there are variables for these values.
+-- Average rating is NULL by default
+INSERT INTO
+    FoodEstablishment (
+        establishment_id,
+        name,
+        street_address,
+        city,
+        province
+    )
+VALUES
+    (
+        establishment_id,
+        establishment_name,
+        street_address,
+        city,
+        province
+    );
+
+-- Delete a food establishment
+DELETE FROM FoodEstablishment
+WHERE
+    establishment_id = establishment_id;
+
+-- Search for a food establishment
+SELECT
+    establishment_id,
+    name,
+    street_address,
+    city,
+    province,
+    rating
+FROM
+    FoodEstablishment
+WHERE
+    establishment_id = establishment_id;
+
+-- Update a food establishment, establishment id is given. 
+-- Assumming there are variables for these values. 
+-- Update establishment name
+UPDATE FoodEstablishment
+SET
+    name = new_name
+WHERE
+    establishment_id = establishment_id;
+
+-- Update location
+UPDATE FoodEstablishment
+SET
+    street_address = new_street_address,
+    city = new_city,
+    province = new_province
+WHERE
+    establishment_id = establishment_id;
+
+-- Update average rating
+UPDATE FoodEstablishment
+SET
+    rating = new_rating
+WHERE
+    establishment_id = establishment_id;
+
+-- 3. Add, delete, search, and update a food item
+-- Add a food item, assumming there are variables for these values.
+-- Average rating is NULL by default
+INSERT INTO
+    FoodItem (item_id, food_name, type, price, establishment_id)
+VALUES
+    (
+        item_id,
+        food_name,
+        food_type,
+        food_price,
+        establishment_id
+    );
+
+-- Delete a food item
+DELETE FROM FoodItem
+WHERE
+    item_id = item_id;
+
+-- Search for a food item
+SELECT
+    item_id,
+    food_name,
+    type,
+    price,
+    establishment_id
+FROM
+    FoodItem
+WHERE
+    item_id = item_id;
+
+-- Update a food item, food id is given. 
+-- Assumming there are variables for these values. 
+-- Update food name
+UPDATE FoodItem
+SET
+    food_name = new_name
+WHERE
+    item_id = item_id;
+
+-- Update food type
+UPDATE FoodItem
+SET
+    type = new_food_type
+WHERE
+    item_id = item_id;
+
+-- Update price
+UPDATE FoodItem
+SET
+    price = new_price
+WHERE
+    item_id = item_id;
 
 -- DML FOR PROJECT REPORTS
 -- 1. View all food establishments
@@ -90,10 +286,10 @@ SELECT * FROM FoodReview WHERE establishment_id=(SELECT establishment_id FROM Fo
 SELECT * FROM FoodReview WHERE item_id=(SELECT item_id FROM FoodItem WHERE food_name=food_name) AND review_month=review_month;
 
 -- 6. View all establishments with a high average rating (rating >= 4) (ratings from 1-5; highest is 5);
-SELECT * FROM FoodEstablishment WHERE establishment_id=(SELECT establishment_id FROM FoodReview WHERE rating>=4);
+SELECT * FROM FoodEstablishment WHERE establishment_id=(SELECT establishment_id FROM FoodReview WHERE AVG(rating)>=4);
 
 -- 7. View all food items from an establishment arranged according to price; 
 SELECT * FROM FoodItem WHERE establishment_id=(SELECT establishment_id FROM FoodEstablishment WHERE name=name) ORDER BY price ASC;
 
 -- 8. Search food items from any establishment based on a given price range and/or food type;
-SELECT * FROM FoodItem WHERE price IN (price, price) AND type=type;
+SELECT * FROM FoodItem WHERE price IN (price, price) OR type=type;
