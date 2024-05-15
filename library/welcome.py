808212ef2eb9d72f8c41e6c30762fc302dec57d3
jtvallente <jtvallente@up.edu.ph>
2024-05-15 10:17:06 +0800

    created welcome page

--- library/welcome.py
+++ library/welcome.py
@@ -5,9 +5,9 @@ def clear_screen():
     # Clear the terminal screen
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def print_customer_menu():
+def print_welcome():
     padding_color = '44'  
-    menu_text = "  C U S T O M E R  M E N U  "
+    menu_text = "  W E L C O M E !  "
     menu_color = '44'  
     reset = '\033[0m'  
 
@@ -20,39 +20,35 @@ def print_customer_menu():
     print(f"\n\033[{padding_color}m{' '*len(menu_text)}{reset}\n")
 
 
-#These 3 functions below are just placeholders. 
+#These 2 functions below are just placeholders. 
 #It will be replaced by the imported functions/libs created by other members
-def add_food_review():
+def login():
     print("Adding food review...")
 
-def update_food_review():
+def signup():
     print("Updating food review...")
 
-def delete_food_review():
-    print("Deleting food review...")
-
-def customer_menu():
+def welcome_page():
     while True:
         clear_screen()  # Clear the screen
-        print_customer_menu()
-        print(Fore.GREEN + "[1] Add food review")
-        print(Fore.YELLOW + "[2] Update food review")
-        print(Fore.RED + "[3] Delete food review")
-        print(Fore.BLUE + "[4] Back to catalog")
+        print_welcome()
+        print("App version: 1.1\n")
+        print(Fore.GREEN + "[1] Login")
+        print(Fore.BLUE + "[2] Create an Account")
+        print("")
+        print(Fore.RED + "[3] Exit")
         print(Style.RESET_ALL)  # Reset colors
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            add_food_review()
+            login()
         elif choice == '2':
-            update_food_review()
+            signup()
         elif choice == '3':
-            delete_food_review()
-        elif choice == '4':
-            print("Returning to catalog...")
+            print("\nProgram Exitting...")
             break
         else:
             print("Invalid choice. Please try again.")
 
 # Example usage
-customer_menu()
+welcome_page()
